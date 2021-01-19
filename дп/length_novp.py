ccbb5a1dc5425363cd287b3fd4bf2b8aa4c30211
VeronicaDem <veron.dem4enko@yandex.ru>
2021-01-19 16:31:40 +0300

    наибольшая возрастающая подпоследовательность

---
+++ дп/length_novp.py
@@ -0,0 +1,22 @@
+def novp(A):
+    F = [0]*len(A)
+    for i in range(len(A)):
+        for j in range(i):
+            if A[j] < A[i] and F[j] > F[i]:
+                F[i] = F[j]
+        F[i]+=1
+
+def novp2(A):
+    INF = 10 ** 10
+    F = [INF] * (len(A) + 1)
+    F[0] = -INF
+    for i in range(len(A)):
+        left = 0
+        right = len(A)
+        while right - left > 1:
+            middle = (left + right) // 2
+            if F[middle] >= A[i]:
+                right = middle
+            else:
+                left = middle
+        F[right] = A[i]

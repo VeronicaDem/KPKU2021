6d07a45eb3d3f5e90899b7e68731081c43066ccc
VeronicaDem <veron.dem4enko@yandex.ru>
2021-01-27 10:49:42 +0300

    binary tree

---
+++ структуры данных/Реализации структур данных/SimpleTree.cpp
@@ -0,0 +1,52 @@
+﻿
+
+#include <iostream>
+using namespace std;
+struct tree {
+	int info;
+	tree* L;
+	tree* R;
+};
+void ADDDDP(tree* p, int x) {
+	tree* q;
+	if (x < p->info) {
+		if (p->L == NULL) {
+			q = new tree;
+			q->info = x;
+			q->L = NULL;
+			q->R = NULL;
+			p->L = q;
+		}
+		else ADDDDP(p->L, x);
+	}
+	else {
+		if (p->R == NULL) {
+			q = new tree;
+			q->info = x;
+			q->L = NULL;
+			q->R = NULL;
+			p->R = q;
+		}
+		else ADDDDP(p->R, x);
+	}
+}
+
+void print(tree* t) {
+	if (t != 0) {
+		cout << t->info << endl;
+
+		print(t->L);
+
+		print(t->R);
+	}
+}
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	tree* t = new tree;
+	int x;
+	cin >> x;
+	t->info = x;
+	t->L = NULL;
+	t->R = NULL;
+}

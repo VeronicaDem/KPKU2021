a72153e40b361729c65874eca29b9d1738a31d14
VeronicaDem <veron.dem4enko@yandex.ru>
2021-02-08 14:28:25 +0300

    add task 4 std::list

---
+++ структуры данных/Задачи/список/UsingSTLListTask4.cpp
@@ -0,0 +1,37 @@
+﻿// UsingSTLListTask4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <list>
+using namespace std;
+bool exist1(int chislo) {
+    while (chislo) {
+        if (chislo % 10 == 1) return true;
+        chislo /= 10;
+    }
+    return false;
+}
+int main()
+{
+    list<int> li;
+    int size;
+    cin >> size;
+    for (int i = 0; i < size; i++) {
+        int info;
+        cin >> info;
+        li.push_back(info);
+    }
+    auto end = prev(li.end(), 2);
+    auto curr = next(li.begin(), 1);
+    while (curr != end) {
+        if (!exist1(*curr) && *next(curr, 1) == 8) {
+            curr = li.erase(curr);
+        }
+        curr++;
+    }
+    if (!exist1(*li.begin()) && *next(li.begin(), 1) == 8) {
+        li.erase(li.begin());
+    }
+    copy(li.begin(), li.end(), ostream_iterator<int>(cout, " "));
+}
+
